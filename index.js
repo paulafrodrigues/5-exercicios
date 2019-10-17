79c3ad63522eb52b5cbb8161617f3b27ed1b31bb
paulafrodrigues <pfr.blackcat@gmail.com>
2019-10-16 22:22:16 -0300

    colocando as perguntas

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+//criar API com express e servidor 3000
+
 const express = require('express');
 const server = express();
 const usuariosLista = ['Paula', 'Thiele', 'Nataly'];
+
+//criar um GET com res de Hello World
 
 server.get('/', (req, res, next) => {
     res.send('Hello World!');
 });
 
+//criar um GET que te dê a lista de usuários
+
 server.get('/usuarios', (req, res, next) =>{
-    res.send(usuariosLista);
+    res.json(usuariosLista);
 });
+
+// Criar GET que te dê um usuário
 
 server.get('/usuarios/:username', (req, res, next) =>{
     res.send(req.params);
